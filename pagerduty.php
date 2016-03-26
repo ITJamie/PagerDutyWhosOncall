b38e53a28cdc3f3c5e01c16164d3fcad2f4bbacc
Jamie Murphy <jamie.murphy@airbnb.com>
2016-03-26 12:46:57 +0000

    init

---
+++ pagerduty.php
@@ -0,0 +1,179 @@
+<?php
+
+
+class PagerDutyWhosOncall {
+
+	public $options;
+    public function __construct( $options=array() ){
+        $default_options = array(
+            'baseurl' => NULL, 
+            'apikey' => NULL,
+            'debug' => false,
+            'timezone' => 'UTC', // This is the timezone that will be passed to pagerduty. https://developer.pagerduty.com/documentation/rest/types#timezone
+        );
+        
+        $this->options = array_merge($default_options, $options);
+
+	}
+	
+	private function apiCall($path, $parameters = null) {
+
+		$context = stream_context_create(array(
+			'http' => array(
+				'header' => "Content-type: application/json",
+				'header'  => "Authorization: Token token=" . $this->options['apikey'])
+			)
+		);
+
+		$params = null;
+		if (isset($parameters)){
+			foreach ($parameters as $key => $value) {
+				if (isset($params)) {
+					$params .= '&';
+				} else {
+					$params = '?';
+				}
+				$params .= sprintf('%s=%s', $key, $value);
+			}
+		}
+	
+		$respose = file_get_contents($this->options['baseurl'] . $path . $params, false, $context);
+		if($this->options['debug'] !="false"){
+			echo $respose;
+		}
+	
+	
+		return  $respose;
+	}
+
+
+
+
+	private function whoIsOnCallOverride($scheduleid, $time = null) {
+		//this function checks to see if any override entries exist. Used by the whoIsOncall function
+   
+	
+		$since = date('c', isset($time) ? $time : time());
+		$until = $since+60; // adds 60 seconds to since. sometimes pagerduty does not give any entries otherwise
+		
+		$parameters = array(
+			'since' => $since,
+			'until' => $until,
+			'overflow' => 'true',
+			'time_zone'=> $this->options['timezone'],
+		);
+	
+		$json = $this->apiCall(sprintf('/schedules/%s/overrides', $scheduleid), $parameters) ;
+		if($this->options['debug'] !="false"){
+			echo 'Json supplied by Pagerduty'.PHP_EOL;
+			print_r($json);
+			echo PHP_EOL;
+		}
+		if (false === ($scheddata = json_decode($json))) {
+			if($this->options['debug'] !="false"){
+				echo "There was an error with the data from PagerDuty, please try again later.\n".PHP_EOL;
+			}
+			return false;
+		}
+	
+		if (isset($scheddata->overrides['0'])){
+			if ($scheddata->overrides['0']->user->name == "") {
+				if($this->options['debug'] !="false"){
+					echo "No data from Pagerduty for that date, sorry.\n".PHP_EOL;
+				}
+				return false;
+			}
+		} else { return false;}
+	
+	
+		$phonenumber = $this->getPhoneNumberForOncall( $scheddata->overrides['0']->user->id );
+
+		$oncalldetails = array();
+		$oncalldetails['id'] = $scheddata->overrides['0']->user->id;
+		$oncalldetails['person'] = $scheddata->overrides['0']->user->name;
+		$oncalldetails['email'] = $scheddata->overrides['0']->user->email;
+		$oncalldetails['phone'] = $phonenumber;
+		$oncalldetails['type'] = 'override oncall rota'; // Declairing the entry type as an override. 
+		$oncalldetails['start'] = strtotime($scheddata->overrides['0']->start);
+		$oncalldetails['end'] = strtotime($scheddata->overrides['0']->end);
+
+		return $oncalldetails;
+
+	}
+
+	public function whoIsOnCall($scheduleid, $timestamp = null) {
+
+		$since = date('c', isset($time) ? $timestamp : time());
+		$until = date('c', isset($time) ? $timetsamp : time()+60);
+		$parameters = array(
+			'since' => $since,
+			'until' => $until,
+			'overflow' => 'true',
+			'time_zone'=> $this->options['timezone'],
+		);
+	
+		$json = $this->apiCall(sprintf('/schedules/%s/entries', $scheduleid), $parameters);
+		if($this->options['debug'] !="false"){
+			echo 'Json supplied by Pagerduty'.PHP_EOL;
+			print_r($json);
+			echo PHP_EOL;
+		}
+		if (false === ($scheddata = json_decode($json))) {
+			if($this->options['debug'] !="false"){
+				echo "There was an error with the data from PagerDuty, please try again later.\n".PHP_EOL;
+			}
+			return false;
+		}
+
+		if ($scheddata->entries['0']->user->name == "") {
+			if($this->options['debug'] !="false"){
+				echo "No data from Pagerduty for that date, sorry.\n".PHP_EOL;
+			}
+			return false;
+		}
+	
+		//check to see if there are any overrides!
+		$overrideoncall= $this->whoIsOnCallOverride($scheduleid);
+		if (is_array($overrideoncall)){
+			return $overrideoncall;
+		}
+		$phonenumber = $this->getPhoneNumberForOncall( $scheddata->entries['0']->user->id );
+
+		$oncalldetails = array();
+		$oncalldetails['id'] = $scheddata->entries['0']->user->id;
+		$oncalldetails['person'] = $scheddata->entries['0']->user->name;
+		$oncalldetails['email'] = $scheddata->entries['0']->user->email;
+		$oncalldetails['phone'] = $phonenumber;
+		$oncalldetails['type'] = 'standard oncall rota';
+		$oncalldetails['start'] = strtotime($scheddata->entries['0']->start);
+		$oncalldetails['end'] = strtotime($scheddata->entries['0']->end);
+
+		return $oncalldetails;
+
+	}
+
+
+
+	// Returns an array of all contact details
+	private function getContactDetails($userid){
+		$json = $this->apiCall("users/".$userid."/contact_methods");
+
+		return(json_decode($json,true) );
+	
+	}
+
+	// return a phone number in full international format
+	private function getPhoneNumberForOncall($userid){
+		$contactarray= $this->getContactDetails($userid);
+		$foundaphone =0;
+		foreach($contactarray['contact_methods'] as $contactdetails){
+
+			if ($contactdetails['type'] == 'phone'){
+				return "+".$contactdetails['country_code'] .$contactdetails['phone_number'] ;
+			}
+		}
+	}
+
+
+
+}
---
+++ readme.md
@@ -0,0 +1,41 @@
+PHP PagerDuty WhosOnCall
+===============
+(c) 2016 Jamie Murphy <jamiemurphyit@gmail.com>
+Installation
+-----------
+``` sh
+$ composer install itjamie/PagerDutyWhosOncall
+```
+
+Basic Usage
+-----------
+
+```php
+	date_default_timezone_set('UTC'); // set the timezone of your php env to be the same you pass to pagerduty
+	$pagerdutyapi = new PagerDutyWhosOncall(array(
+            'baseurl' => 'https://instancename.pagerduty.com/api/v1/', 
+            'apikey' => 'placeholderforyourapi',
+            'debug' => false,
+            'timezone' = 'UTC' // This is the timezone that will be passed to pagerduty. https://developer.pagerduty.com/documentation/rest/types#timezone
+            )
+	);
+        
+	$scheduleid = "abc123";
+
+
+	echo ' Oncall Now :'.PHP_EOL;
+	$result = $pagerdutyapi->whoIsOnCallOverride( $scheduleid );
+    var_dump($result);
+
+	echo ' Oncall in one hour :'.PHP_EOL;
+	$time = time()+60*60;
+	$result = $pagerdutyapi->whoIsOnCallOverride( $scheduleid, $time );
+    var_dump($result);
+
+```
+
+
+Credits
+-----------
+
+Thanks to ozzd/PagerdutyPHP for some of the original code. 
